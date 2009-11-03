fb580a9bd72647f4d19c14d1f0bdea29d9de2ed2
fiddlerpianist <fiddlerpianist@633f67f2-f795-11dd-a295-0b7b0935bf94>
2009-11-03 16:43:48 +0000

    updated to verion 0.6 schema

--- knitml/trunk/knitml-el/src/test/groovy/com/knitml/el/KelUtils.groovy
+++ knitml/trunk/knitml-el/src/test/groovy/com/knitml/el/KelUtils.groovy
@@ -3,6 +3,9 @@ package com.knitml.el
 import org.apache.commons.lang.StringUtilsimport com.knitml.core.common.Parameters
 import com.knitml.el.GroovyKnitProgram
 
+import static com.knitml.core.xml.Schemas.CURRENT_PATTERN_SCHEMA
+import static com.knitml.core.model.Version.getCurrentVersionId
+
 class KelUtils {
 	
 	protected static String toXml(String kel) {
@@ -12,7 +15,7 @@ class KelUtils {
 		GroovyKnitProgram converter = new GroovyKnitProgram()
 		converter.convertToXml(parameters)
 		String value = parameters.writer.toString()
-		value = StringUtils.remove(value, '<pattern xmlns="http://www.knitml.com/schema/pattern" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.knitml.com/schema/pattern http://www.knitml.com/schema/pattern-0.5.xsd" version="0.5">')
+		value = StringUtils.remove(value, '<pattern xmlns="http://www.knitml.com/schema/pattern" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.knitml.com/schema/pattern ' + CURRENT_PATTERN_SCHEMA + '" version="' + getCurrentVersionId() + '">')
 		value = StringUtils.remove(value, '</pattern>')
 		return value
 	}
