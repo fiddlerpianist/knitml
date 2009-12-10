3d9feb711e227e05ef10d733191f4a496fda9cca
fiddlerpianist <fiddlerpianist@633f67f2-f795-11dd-a295-0b7b0935bf94>
2009-12-10 17:31:34 +0000

    removed erroneous javadoc comment

--- knitml/trunk/knitml-core/src/main/java/com/knitml/core/model/directions/StitchNatureProducer.java
+++ knitml/trunk/knitml-core/src/main/java/com/knitml/core/model/directions/StitchNatureProducer.java
@@ -1,10 +1,6 @@
 package com.knitml.core.model.directions;
 
 /**
- * An interface which designates that the inline operation knows, at any point
- * of usage, how many stitches it uses on the needle and how many stitches it
- * causes to be increased / decreased.
- * 
  * @author Jonathan Whitall
  * 
  */
