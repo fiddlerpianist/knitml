3e2211f72d190721060b3cab5d2db9362fc0c6f7
fiddlerpianist <fiddlerpianist@gmail.com>
2011-11-16 04:40:33 +0000

    added various constructors

--- knitml/trunk/knitml-core/src/main/java/com/knitml/core/model/header/GeneralInformation.java
+++ knitml/trunk/knitml-core/src/main/java/com/knitml/core/model/header/GeneralInformation.java
@@ -37,4 +37,28 @@ public class GeneralInformation {
 	public String getLanguageCode() {
 		return languageCode;
 	}
+	public void setPatternName(String patternName) {
+		this.patternName = patternName;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public void setLanguageCode(String languageCode) {
+		this.languageCode = languageCode;
+	}
+	public void setDimensions(String dimensions) {
+		this.dimensions = dimensions;
+	}
+	public void setTechniques(List<String> techniques) {
+		this.techniques = techniques;
+	}
+	public void setGauge(Gauge gauge) {
+		this.gauge = gauge;
+	}
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+	public void setCopyright(String copyright) {
+		this.copyright = copyright;
+	}
 }
